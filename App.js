f2ae302be654e0312961cb2541d1b94ced627487
girikgarg8 <girikgarg8@gmail.com>
2023-01-10 13:54:58 +0530

    Igniting the React App

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react"; //getting from node modules
+import ReactDOM from "react-dom/client";
 const heading1=React.createElement("h1",{
                 id:"title",
-            }," Heading 1") // second one is object, React element is an object at the end of the day: second parameter is equivalent to specifying div id="title", second one is like id="title"
+                hellow:"hello"
+            }," Heading l for parcel") // second one is object, React element is an object at the end of the day: second parameter is equivalent to specifying div id="title", second one is like id="title"
             //console.log(heading) #React element is an object
             const heading2 = React.createElement("h2", {
                     id: "title",
@@ -12,3 +15,4 @@ const heading1=React.createElement("h1",{
             )
             const root=ReactDOM.createRoot(document.getElementById("root")) //getElementByID is used to get the div which has the ID root at line number 10
             root.render(container) // passing a react element inside the root
+            //dist folder stores the minified files 
