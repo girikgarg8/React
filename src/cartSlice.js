1e4356b4603f874b97236372f6286ed45e149f02
girikgarg8 <girikgarg8@gmail.com>
2023-02-09 15:41:02 +0530

    1:29:49

--- src/cartSlice.js
+++ src/cartSlice.js
@@ -3,9 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice=createSlice({
     name:'cart',
     initialState:{
-        items:[] //initialising an empty cart 
+        items:["Apples","Banana"] //initialising an empty cart 
     },
     reducers:{ //reducer is that function which updates the value in the store
-
+        addItem:(state,action)=>{ //the reducer takes a state and action, the state always points to the previous state
+            state.items.push(action.payload) 
+        }
     }
 })
