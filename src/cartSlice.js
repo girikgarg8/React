3fcc7e6b1f95838a8c30b4f9f81aca18c658078f
girikgarg8 <girikgarg8@gmail.com>
2023-02-09 13:47:42 +0530

    1:27:57

---
+++ src/cartSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice=createSlice({
+    name:'cart',
+    initialState:{
+        items:[] //initialising an empty cart 
+    },
+    reducers:{ //reducer is that function which updates the value in the store
+
+    }
+})
